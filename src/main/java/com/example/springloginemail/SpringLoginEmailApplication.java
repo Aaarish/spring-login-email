9386562782e7bc9475223fcc55c819db49fe6cd2
Aarish <aarishm767@gmail.com>
2023-01-26 16:49:56 +0530

    Initial Commit

---
+++ src/main/java/com/example/springloginemail/SpringLoginEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.springloginemail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoginEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoginEmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springloginemail/SpringLoginEmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springloginemail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoginEmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
